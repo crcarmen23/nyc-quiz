d7d04e6577db86ca22ab7975e9b5c0e717e174ce
Carmen Regina <crcarmen23@gmail.com>
2023-09-25 10:03:33 -0500

    set up basic index.html structure, added quiz questions and answers to js file

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+
+var myQuestions = [{
+    questionText: "How many boroughs are in New York City?",
+    choices: ["4", "5", "6", "3"],
+    correctAnswer: "5"
+}, {
+    questionText: "What is the name of the largest urban park in Manhattan?",
+    choices: ["Bryant Park", "Madison Square Park", "Central Park", "Washington Square Park"],
+    correctAnswer: "Central Park"
+}, {
+    questionText: "In which part of Manhattan can you find Times Square?",
+    choices: ["Soho", "Midtown", "Lower East Side", "Financial District"],
+    correctAnswer: "Midtown"
+}, {
+    questionText: "What is the nickname of New York City?",
+    choices: ["The City of Lights", "The Windy City", "Sin City", "The Big Apple"],
+    correctAnswer: "The Big Apple"
+}, {
+    questionText: "Which street or avenue is home to the New York Stock Exchange?",
+    choices: ["Canal Street", "Wall Street", "Fifth Avenue", "Park Avenue"],
+    correctAnswer: "Wall Street"
+}]
+
+
+
+
+myQuestions[0].questionText
+myQuestions[0].choices[0]
+myQuestions[0].correctAnswer
+
+//on click 
+myQuestions[1]
